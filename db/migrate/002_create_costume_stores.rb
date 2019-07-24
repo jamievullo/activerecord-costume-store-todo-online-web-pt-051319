242fd376c2c5aa37d51d6307ec9e9dce4291316d
Jamie Vullo <jvullo@protonmail.com>
2019-07-24 15:16:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,15 @@
 # Create your costume_stores migration here
-class CreateCostumeStores < ActiveRecord::Migration[5.2]
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.string :still_in_business
+      t.string :opening_time
+      t.string :closing_time
+  end
+  
+end 
