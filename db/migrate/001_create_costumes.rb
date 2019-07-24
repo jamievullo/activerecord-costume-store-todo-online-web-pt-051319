2dd0662ed766cc5c68e3f16c8a64e561ac2138c5
Jamie Vullo <jvullo@protonmail.com>
2019-07-24 15:12:36 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,13 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
   
   def change 
-    
+    create_table :costumes do |t|
+      t.string :name 
+      t.integer :price 
+      t.string :size 
+      t.string :image_url
+      t.timestamp :created_at
+      t.timestamp :updated_at
   end
   
 end 
